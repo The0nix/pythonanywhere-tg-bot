470c6cb06a04ddc82fccd31d2b7366566be9a444
The0nix <nektonikto999@gmail.com>
2019-10-17 15:17:23 +0300

    Add manual slap

--- handlers.py
+++ handlers.py
@@ -8,6 +8,13 @@ from telegram.ext.filters import Filters
 logger = logging.getLogger(__name__)
 
 WHO_REGEX = re.compile(os.environ['SLAPBOT_REGEX'], re.IGNORECASE)
+ONIXINO_ID = 124147029
+TASHA_ID = 11436017
+
+def get_username(user):
+    return ('@{}'.format(user.username)
+            if user.username
+            else user.first_name)
 
 
 def start_callback(update, context):
@@ -30,7 +37,23 @@ def who_callback(update, context):
         pass
 
 
+def manual_slap_callback(update, context):
+    try:
+        is_onix = update.effective_user.id == ONIXINO_ID
+        reply_user = get_username(update.message.reply_to_message.from_user)
+        message = update.message.reply_to_message if is_onix else update.effective_message
+        action = 'love' if message.from_user.id == TASHA_ID else 'slap'
+        text = '/{} {}'.format(action, reply_user) if is_onix else 'Nope'
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=text,
+                                 reply_to_message_id=message.message_id)
+    except Exception as e:
+        logger.error(e)
+        pass
+
+
 handlers = [
     CommandHandler('start', start_callback),
+    CommandHandler('manual_slap', manual_slap_callback),
     MessageHandler(Filters.regex(WHO_REGEX), who_callback),
 ]
